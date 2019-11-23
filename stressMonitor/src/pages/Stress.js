f95f929ff1abe632c0426e22b44f332d1eb5f7ff
EliveltonR18 <elivelton.rodrigues@ufrpe.br>
2019-11-23 16:54:53 -0300

    [master] | some fixes

--- stressMonitor/src/pages/Stress.js
+++ stressMonitor/src/pages/Stress.js
@@ -78,7 +78,7 @@ class Stress extends Component {
                     checkedTingling,
                     commenter,
                     uid: currentUser,
-                    createdAt: new Date(2017, 3, 19),
+                    createdAt: new Date(),
                 }).then(payload => {
                     this.initialState();
                     this.props.navigation.navigate('History');
